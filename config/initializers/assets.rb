4d1fa2c004d24a54035ff5675af0bc992d241720
Zorluhan Zorlu <zorluhan@gmail.com>
2016-02-23 21:02:33 +0200

    added images

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,7 +10,7 @@ Rails.application.config.assets.precompile += %w( bootstrap.css )
 Rails.application.config.assets.precompile += %w( style.css )
 
 
-Rails.application.config.assets.precompile += %w( capish.png )
+Rails.application.config.assets.precompile += %w( *.png *.jpg)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
